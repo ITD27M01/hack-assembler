9e3b171f3763f939ed8378975b36bbc50eb28d9f
ITD27M01 <igortiunov@gmail.com>
2020-05-18 12:51:47 +0300

    Fixed error for empty argument for file path

--- assembler/arguments.py
+++ assembler/arguments.py
@@ -3,7 +3,7 @@ import argparse
 
 def args_parser():
     parser = argparse.ArgumentParser(description='Generates hack machine binary code from symbolic form.')
-    parser.add_argument('file', nargs='?', type=str, action='store',
+    parser.add_argument('file', type=str, action='store',
                         help='File path with assembly code')
 
     parser.add_argument('--debug', default=False, action='store_true',
