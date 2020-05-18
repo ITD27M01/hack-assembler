cc2030badcb38a7997389a07340f458c827e8b52
ITD27M01 <igortiunov@gmail.com>
2020-05-18 12:22:15 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='assembler',
-    version='0.1.0',
+    version='0.1.1',
     author="itd27m01",
     author_email="igortiunov@gmail.com",
     description="nand2tetris hack assembler",
